3e4076392c8b288d2633babf735852c5adb8f88c
Rajibul <rajibridoy884@gmail.com>
2022-07-05 18:05:04 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from lcd import lcd
 from wifimngr import wifi
+from local_server import server
 
 wifi.connect()
-lcd.putstr("Hello World")
+lcd.putstr("Hello World\n")
+lcd.putstr(wifi.wlan_sta.ifconfig()[0])
+gc.collect()
+server.run()
